ec7f4aac693764ab7478c2efc884777f4de0b8bd
Hy3n4 <hy3nk4@gmail.com>
2023-11-28 23:58:31 +0100

    feat(init): add __init__.py

--- custom_components/samsung_soundbar/__init__.py
+++ custom_components/samsung_soundbar/__init__.py
@@ -1,1 +1,53 @@
 """Samsung Soundbar Custom Component."""
+import asyncio
+import logging
+
+from homeassistant import config_entries, core
+
+from .const import DEFAULT_PORT, DOMAIN
+from .media_player import SoundbarAPI
+
+
+async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
+    """Set up soundbar from a config entry."""
+    # Perform setup for the config entry, like connecting to the soundbar or loading platforms
+    hass.data.setdefault(DOMAIN, {})
+    hass_data = dict(entry.data)
+    unsub_options_update_listener = entry.add_update_listener(options_update_listener)
+    hass.data["unsub_options_update_listener"] = unsub_options_update_listener
+    hass.data[DOMAIN][entry.entry_id] = hass_data
+
+    # Load other platforms like light, switch, media_player, etc.
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "media_player")
+    )
+    # Return boolean to indicate that initialization was successful
+    return True
+
+
+async def options_update_listener(hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(config_entry.entry_id)
+
+
+async def async_unload_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[hass.config_entries.async_forward_entry_unload(entry, "media_player")]
+        )
+    )
+    # Remove options_update_listener.
+    hass.data[DOMAIN][entry.entry_id]["unsub_options_update_listener"]()
+
+    # Remove config entry from domain.
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+    """Set up the GitHub Custom component from yaml configuration."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
